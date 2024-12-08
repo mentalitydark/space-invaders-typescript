75ba0a51744c20d92874f027a5b13deb0798e2c2
Guilherme Oliveira <guilherme.oliveira.01@outlook.com>
2024-12-08 17:34:48 -0300

    add prop type in ICharacter

--- src/interfaces/Character.ts
+++ src/interfaces/Character.ts
@@ -1,7 +1,9 @@
+import { TCharacterType } from "../enums"
 import { IGameObject, IGameObjectProps } from "./GameObject"
 import { IHealth } from "./Health"
 
 export interface ICharacter extends IGameObject {
+  type: TCharacterType
   name: string
   health: IHealth
 }
