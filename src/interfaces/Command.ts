4ef7e7843844da2856b4b4a1fd811fd2565f332a
Guilherme Oliveira <guilherme.oliveira.01@outlook.com>
2024-12-08 22:09:30 -0300

    change interface Command

--- src/interfaces/Command.ts
+++ src/interfaces/Command.ts
@@ -1,11 +1,7 @@
-import { ECommandType } from "../enums"
-
 export interface ICommand {
   keys: Record<string, boolean>
-  type: ECommandType
 }
 
 export interface ICommandProps {
   keys: Record<string, boolean>
-  type: ECommandType
 }
