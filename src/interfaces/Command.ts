a6ed0662c5e84cbd2cda5044ed334a6bba2c3761
Guilherme Oliveira <guilherme.oliveira.01@outlook.com>
2024-12-10 22:55:10 -0300

    add enum in interface Command

--- src/interfaces/Command.ts
+++ src/interfaces/Command.ts
@@ -1,7 +1,11 @@
+import { EGameState } from "../enums"
+
 export interface ICommand {
   keys: Record<string, boolean>
+  gameState: EGameState
 }
 
 export interface ICommandProps {
   keys: Record<string, boolean>
+  gameState: EGameState
 }
