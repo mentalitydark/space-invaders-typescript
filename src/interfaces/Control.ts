83d077c49707716f15cca5aee0399139840f233e
Guilherme Oliveira <guilherme.oliveira.01@outlook.com>
2024-12-10 22:54:56 -0300

    add game in interface Control

--- src/interfaces/Control.ts
+++ src/interfaces/Control.ts
@@ -1,10 +1,16 @@
 import { ICommand } from "./Command"
+import { IGame } from "./Game"
 import { IObserver } from "./Observer"
 
 export interface IControl {
+  game: IGame
   keys: Record<string, boolean>
   observers: Map<string, IObserver>
   addObserver(observer: IObserver): void
   removeObserver(observer: IObserver): void
   notifyObservers(command: ICommand): void
+}
+
+export interface IControlProps {
+  game: IGame
 }
