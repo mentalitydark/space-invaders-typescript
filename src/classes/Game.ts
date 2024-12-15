26338298f9e2b35ad1836c44da8fb5645a92eaf1
Guilherme Oliveira <guilherme.oliveira.01@outlook.com>
2024-12-14 21:23:49 -0300

    update class Game with prop display & add render

--- src/classes/Game.ts
+++ src/classes/Game.ts
@@ -1,5 +1,5 @@
 import { EGameState } from "../enums";
-import { IControl, IDimension, IFramePerSeconds, IGame, IGameProps, IPlayer } from "../interfaces";
+import { IControl, IDimension, IDisplay, IFramePerSeconds, IGame, IGameProps, IPlayer } from "../interfaces";
 import { Control } from "./Control";
 import { Dimension } from "./Dimension";
 import { FramePerSeconds } from "./FramePerSeconds";
@@ -11,6 +11,7 @@ export class Game implements IGame {
   private _state: EGameState = EGameState.NOT_STARTED
   private _screen: IDimension
   private _FPS: IFramePerSeconds
+  private _display: IDisplay|null = null
 
   public constructor({ player, screen }: IGameProps) {
     this._player = player
@@ -29,6 +30,7 @@ export class Game implements IGame {
   public start(): void {
     const loop = () => {
       this.update()
+      this.render()
 
       requestAnimationFrame(loop.bind(this))
     }
@@ -36,14 +38,24 @@ export class Game implements IGame {
     requestAnimationFrame(loop.bind(this))
   }
 
+  public render(): void {
+    if (!this._display) {
+      return
+    }
+
+    this._display.render()
+  }
+
   get player(): IPlayer { return this._player }
   get control(): IControl { return this._control }
   get state(): EGameState { return this._state }
   get screen(): IDimension { return this._screen }
   get FPS(): IFramePerSeconds { return this._FPS }
+  get display(): IDisplay|null { return this._display }
 
   set player(player: IPlayer) { this._player = player }
   set control(control: IControl) { this._control = control }
   set state(state: EGameState) { this._state = state }
   set screen(screen: IDimension) { this._screen = screen }
+  set display(display: IDisplay|null) { this._display = display }
 }
